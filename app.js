8b9ef384b5749ac8ee080276aa44b662c3d8d3cf
Ivan Khromov <khromov.ivan@gmail.com>
2018-01-24 10:15:50 +0500

    Add minimal express app

--- app.js
+++ app.js
@@ -5,3 +5,7 @@
 
 const express = require('express')
 let app = express()
+
+app.get('/', (req, res) => res.send('Hi, adisys!'))
+
+app.listen(7421, () => console.log('ADiSys listening on port 3000!'))
