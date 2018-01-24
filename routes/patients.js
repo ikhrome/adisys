e6c937304df7d8756a7e19f984f20b228cd85e9a
ikhrome <khromov.ivan@gmail.com>
2018-01-24 23:43:59 +0500

    Fix some lint errors

--- routes/patients.js
+++ routes/patients.js
@@ -5,16 +5,16 @@ const logger  = require('winston')
 // Import all models!
 // If I use ref fields, I need to import all of this here:
 const Patient = require('../models/patient')
-const Workplace = require('../models/workplace')
+require('../models/workplace')
 
 router.get('/list', function(req, res) {
 	Patient.find()
-	.populate('workplace')
-	.exec((err, pats) => {
-		res.render('patients/list', {
-			patients: pats
+		.populate('workplace')
+		.exec((err, pats) => {
+			res.render('patients/list', {
+				patients: pats
+			})
 		})
-	})
 })
 
 module.exports = router
