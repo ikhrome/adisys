26b13ce76f85c792e97d03d9afcc82845dd7d461
Ivan Khromov <khromov.ivan@gmail.com>
2018-01-25 09:36:18 +0500

    Fix lint error. Fucking spaces in Atom =\

--- misc/logger.js
+++ misc/logger.js
@@ -2,14 +2,14 @@ var winston = require('winston')
 var adiLogger = new winston.Logger()
 
 adiLogger.add(winston.transports.Console, {
-  level: 'debug',
-  colorize: true
+	level: 'debug',
+	colorize: true
 })
 
 adiLogger.add(winston.transports.File, {
-  level: 'error',
-  filename: 'logs/errors.log',
-  json: true
+	level: 'error',
+	filename: 'logs/errors.log',
+	json: true
 })
 
 module.exports = adiLogger
