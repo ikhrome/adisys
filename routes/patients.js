8738da1ae28d6a9ee517a9dfe6529c4c1cad00a0
Ivan Khromov <khromov.ivan@gmail.com>
2018-01-31 11:35:24 +0500

    FIX code style

--- routes/patients.js
+++ routes/patients.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router  = express.Router()
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 // Import all models!
 // If I use ref fields, I need to import all of this here:
@@ -19,7 +19,7 @@ router.get('/list', function(req, res) {
 
 router.get('/card/:id', function(req, res) {
 	if(!mongoose.Types.ObjectId.isValid(req.params.id))
-		res.status(403).send("Nope! This is not valid MongoDB ObjectId!")
+		res.status(403).send('Nope! This is not valid MongoDB ObjectId!')
 	Patient.findOne({ _id: req.params.id }, function(err, patient) {
 		res.render('patients/card', { patient: patient })
 	})
